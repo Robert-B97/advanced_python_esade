f46a5d1a6fbbfef8048f24b9a1079ce4329fcb17
Robert Bernasconi <robert.bernasconi@alumni.esade.edu>
2022-05-25 14:48:40 +0200

    created basic classes and functions

--- assignment_robert_bernasconi.py
+++ assignment_robert_bernasconi.py
@@ -30,23 +30,71 @@
 
 # Architectur/execution model
 
+#MAYBE:
+#FIFO
+#expiration date
+#array() instead of quantity
+#Have two wearhouses were the stock can be shared between them.
+#Create a report of all the stock available in the wearhouse.
+
+#TO DO:
+# exceptions handling
+# input parameterds
+# create unittest
 
 
-#class Stock:
 
 
-#class Stock1(Stock):
+class Product:
+    def __init__(self,current_amount: int = 0):
+        self.current_amount = current_amount
 
-#class Stock2(Stock):
+class Pineapples(Product):
+    def __init__(self):
+        super().__init__()
+class Oranges(Product):
+    def __init__(self):
+        super().__init__()
+        pass
 
-#class Stock3(Stock):
+class Watermelon(Product):
+    def __init__(self):
+        super().__init__()
+        pass
+
+class Warehouse:
+    def __init__(self,maximum_capacity: int):
+        self.maximum_capacity = maximum_capacity
+        self.pineapples = Pineapples()
+        self.oranges = Oranges()
+        self.watermelon = Watermelon()
+
+    def add_stock(self,quantity,stock_type):
+        stock_type.current_amount += quantity
+
+    def remove_stock(self,quantity,stock_type):
+        stock_type.current_amount -= quantity
+
+    def get_stock_amount(self,stock_type):
+        return stock_type.current_amount
+
+    def is_stock_available(self,stock_type,required_amount:int = 0) -> bool:
+        return self.get_stock_amount(stock_type) >= required_amount
+
+    def get_report(self) -> dict:
+        return [(i, x.__dict__[i].__dict__['current_amount']) for i in list(self.__dict__.keys())[1:]]
 
 
-#class Warehouse:
 
 
+    
 
 
 
 if __name__ == "__main__":
-    print(1)
+    x = Warehouse(100)
+    '''x.add_stock(5,x.pineapples)
+    x.remove_stock(3,x.pineapples)
+    print(x.pineapples.current_amount)
+    print(x.is_stock_available(x.pineapples,3))'''
+    print(x.get_report())
